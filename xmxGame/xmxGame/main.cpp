b7409f9a2414c58ff05693c9d7d556ec2ec70fd8
xxMeow <xmx1025@gmail.com>
2019-10-09 19:00:17 +0900

    Cleaned framework

--- xmxGame/xmxGame/main.cpp
+++ xmxGame/xmxGame/main.cpp
@@ -39,11 +39,13 @@ b2Vec2 gravity(0.0f, -10.0f);
 b2World world(gravity);
 void worldRun();
 
+/** Simulation settings **/
+float32 timeStep = 1.0f / 60.0f;
+int32 velocityIterations = 6;
+int32 positionIterations = 2;
+
 /** Render functions **/
 GLFWwindow *window;
-int beforeRender();
-int inRender(Program shader);
-int afterRender();
 
 /** Setup vertex data **/
 // Each vertex includes 3 coordinates (x, y, z:depth), the middle point of space is (0.0, 0.0, 0.0)
@@ -61,13 +63,161 @@ int main(int argc, const char *argv[])
     
     
     /** Render **/
-    beforeRender();
+    // Initialize GLFW
+    glfwInit();
+    
+    /** Set basic context **/
+    // Set OpenGL version number as 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Use the core profile
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // MacOS is forward compatible
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    
+    /** Create a GLFW window **/
+    window = glfwCreateWindow(cam.width, cam.height, "xmxOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    // Set the context of this window as the main context of current thread
+    glfwMakeContextCurrent(window);
+    
+    // Initialize GLAD : this should be done before using any openGL function
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD." << std::endl;
+        glfwTerminate(); // This line isn't in the official source code, but I think that it should be added here.
+        return -1;
+    }
+    
+    
+    /** Callback Functions **/
+    // Callback functions should be registered after creating window and before initializing render loop
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    draw.Create();
+    
+    /** ---------------------------------- Simulation & Rendering ---------------------------------- **/
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Rendering
     Program shader("../Shaders/VertexShader.glsl", "../Shaders/FragmentShader.glsl");
     std::cout << "Program ID: " << shader.ID << std::endl;
-    inRender(shader);
-    afterRender();
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    
+    // Bind the VAO
+    glBindVertexArray(VAO);
+    // Bind the VBO as GL_ARRAY_BUFFER
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // glBufferData() will copy datas to the buffer which is being bound right now (here it's VBO!)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    /** glVertexAttribPointer() will get each vertex attributes from the VBO
+     *  void glVertexAttribPointer(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const GLvoid * pointer);
+     **/
+    // Position Pointer
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // Color Pointer
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
+    /** Unbind **/
+    // Since glAttribArrayPointer() has registered VBO as the vertex attributes' bound vertex buffer object, now we can unbind it safely
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // Similar to the VBO, the VAO can be also safely unbound now so that this VAO won't get accidentally modified by other VAO calls
+    // However, modifying other VAOs required a call to glBindVertexArray() anyways so it's okay that don't do the unbinding to VAOs (but not VBOs) when it's not directly necessary
+    glBindVertexArray(0);
+    
+    // Uncomment this call to draw in wireframe polygons
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // Option: GL_FILL(default), GL_POINT, GL_LINE
+        
+    int i = 0;
+    b2Body* groundPtr = world.GetBodyList()->GetNext();
+    b2Body* boxPtr = world.GetBodyList()->GetNext();
+    // Render loop
+    while (!glfwWindowShouldClose(window)) {
+        // Check for events
+        processInput(window);
+        
+        /** Other rendering operations **/
+        
+        // Set background clolor
+        glClearColor(0.2f, 0.2f, 0.5f, 1.0f); // Set color value (R,G,B,A) - Set Status
+        glClear(GL_COLOR_BUFFER_BIT); // Use the color to clear screen - Use Status
+        
+        
+        
+        
+        
+        world.Step(timeStep, velocityIterations, positionIterations);
+
+        // Rendering
+        b2Vec2 groundPos = groundPtr->GetPosition();
+        b2Vec2 boxPos = boxPtr->GetPosition();
+        float32 groundAngle = groundPtr->GetAngle();
+        float32 boxAngle = boxPtr->GetAngle();
+        printf(" >> %d ---------------------\n", i);
+        printf("G: %4.2f %4.2f %4.2f\n", groundPos.x, groundPos.y, groundAngle);
+        printf("B: %4.2f %4.2f %4.2f\n", boxPos.x, boxPos.y, boxAngle);
+        
+        
+        
+        
+        /** Render triangle **/
+        glUseProgram(shader.ID);
+        
+        glBindVertexArray(VAO);
+        // In fact, we don't need to bind the VAO every render loop here since we only have a single VAO. We just do it so to keep things a bit organized
+        
+        // Tell OpenGL to draw 6 vertices whose indices are stored in the VBO
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawElements() will access the elements in the VAO which is being bound right now
+        
+        //glBindVertexArray(0); // Again, we don't need to unbind it right now
+        
+        // Swap color buffers since double buffers are applied for rendering
+        /** After updating my MacOS to Mojave10.14.6, this function dose not cause flickering now **/
+        glfwSwapBuffers(window);
+        // Update the status of window
+        glfwPollEvents();
+        i ++;
+    }
+    
+    // Optional: De-allocate all resources once they've outlived their purpoose
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+//    glDeleteBuffers(1, &EBO);
+
+
+
+
+
+
+
+
+
+
+    /** ---------------------------------- Simulation & Rendering ---------------------------------- **/
     
     draw.Destroy();
+    glfwTerminate();
     
     return 0;
 }
@@ -109,6 +259,8 @@ void processInput(GLFWwindow *window)
     }
 }
 
+
+
 void worldRun()
 {
     /** Ground **/
@@ -144,142 +296,17 @@ void worldRun()
     /** Simulation **/
     // Prepare for simulation. Typically we use a time step of 1/60 of a second (60Hz) and 10 iterations.
     // This provides a high quality simulation in most game scenarios.
-    float32 timeStep = 1.0f / 60.0f;
-    int32 velocityIterations = 6;
-    int32 positionIterations = 2;
+    
     // This is our little game loop.
-    for (int32 i = 0; i < 60; ++i)
-    {
-        // Instruct the world to perform a single step of simulation.
-        // It is generally best to keep the time step and iterations fixed.
-        world.Step(timeStep, velocityIterations, positionIterations);
-
-        // Rendering
-        b2Vec2 position = box->GetPosition();
-        float32 angle = box->GetAngle();
-        printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
-    }
-}
-
-int beforeRender()
-{
-    /** Initialize GLFW **/
-    glfwInit();
-    
-    /** Set basic context **/
-    // Set OpenGL version number as 3.3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    // Use the core profile
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    // MacOS is forward compatible
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-    
-    /** Create a GLFW window **/
-    window = glfwCreateWindow(cam.width, cam.height, "xmxOpenGL", NULL, NULL);
-    if (window == NULL) {
-        std::cout << "Failed to create GLFW window." << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    // Set the context of this window as the main context of current thread
-    glfwMakeContextCurrent(window);
-    
-    // Initialize GLAD : this should be done before using any openGL function
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD." << std::endl;
-        glfwTerminate(); // This line isn't in the official source code, but I think that it should be added here.
-        return -1;
-    }
-    
-    
-    /** Callback Functions **/
-    // Callback functions should be registered after creating window and before initializing render loop
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    
-    draw.Create();
-    
-    return 0;
-}
-
-int inRender(Program shader)
-{
-    
-    unsigned int VAO, VBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    
-    // Bind the VAO
-    glBindVertexArray(VAO);
-    // Bind the VBO as GL_ARRAY_BUFFER
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // glBufferData() will copy datas to the buffer which is being bound right now (here it's VBO!)
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    
-    /** glVertexAttribPointer() will get each vertex attributes from the VBO
-     *  void glVertexAttribPointer(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const GLvoid * pointer);
-     **/
-    // Position Pointer
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // Color Pointer
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    
-    /** Unbind **/
-    // Since glAttribArrayPointer() has registered VBO as the vertex attributes' bound vertex buffer object, now we can unbind it safely
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    // Similar to the VBO, the VAO can be also safely unbound now so that this VAO won't get accidentally modified by other VAO calls
-    // However, modifying other VAOs required a call to glBindVertexArray() anyways so it's okay that don't do the unbinding to VAOs (but not VBOs) when it's not directly necessary
-    glBindVertexArray(0);
-    
-    // Uncomment this call to draw in wireframe polygons
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // Option: GL_FILL(default), GL_POINT, GL_LINE
-    
-    // Render loop
-    while (!glfwWindowShouldClose(window)) {
-        // Check for events
-        processInput(window);
-        
-        /** Other rendering operations **/
-        
-        // Set background clolor
-        glClearColor(0.2f, 0.2f, 0.5f, 1.0f); // Set color value (R,G,B,A) - Set Status
-        glClear(GL_COLOR_BUFFER_BIT); // Use the color to clear screen - Use Status
-        
-        /** Render triangle **/
-        glUseProgram(shader.ID);
-        
-        glBindVertexArray(VAO);
-        // In fact, we don't need to bind the VAO every render loop here since we only have a single VAO. We just do it so to keep things a bit organized
-        
-        // Tell OpenGL to draw 6 vertices whose indices are stored in the VBO
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        // glDrawElements() will access the elements in the VAO which is being bound right now
-        
-        //glBindVertexArray(0); // Again, we don't need to unbind it right now
-        
-        // Swap color buffers since double buffers are applied for rendering
-        /** After updating my MacOS to Mojave10.14.6, this function dose not cause flickering now **/
-        glfwSwapBuffers(window);
-        // Update the status of window
-        glfwPollEvents();
-    }
-    
-    // Optional: De-allocate all resources once they've outlived their purpoose
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-//    glDeleteBuffers(1, &EBO);
-    
-    return 0;
-}
-
-int afterRender()
-{
-    
-    glfwTerminate();
-    
-    return 0;
+//    for (int32 i = 0; i < 60; ++i)
+//    {
+//        // Instruct the world to perform a single step of simulation.
+//        // It is generally best to keep the time step and iterations fixed.
+//        world.Step(timeStep, velocityIterations, positionIterations);
+//
+//        // Rendering
+//        b2Vec2 position = box->GetPosition();
+//        float32 angle = box->GetAngle();
+//        printf("[%d] %4.2f %4.2f %4.2f\n", i, position.x, position.y, angle);
+//    }
 }
